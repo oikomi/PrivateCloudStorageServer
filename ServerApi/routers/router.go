39b17c954f21652f660db11c3fc947b65b1edf60
oikomi <miaohonghit@gmail.com>
2015-03-14 03:37:26 +0800

    init

--- ServerApi/routers/router.go
+++ ServerApi/routers/router.go
@@ -8,23 +8,9 @@
 package routers
 
 import (
-	"github.com/oikomi/PrivateCloudStorageServer/ServerApi/controllers"
 
-	"github.com/astaxie/beego"
 )
 
 func init() {
-	ns := beego.NewNamespace("/v1",
-		beego.NSNamespace("/object",
-			beego.NSInclude(
-				&controllers.ObjectController{},
-			),
-		),
-		beego.NSNamespace("/user",
-			beego.NSInclude(
-				&controllers.UserController{},
-			),
-		),
-	)
-	beego.AddNamespace(ns)
+
 }
