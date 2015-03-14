1a16e8e2e39832561252a09de1ed711509c2295e
oikomi <miaohonghit@gmail.com>
2015-03-15 02:59:55 +0800

    init

---
+++ ServerApi/conf/const.go
@@ -0,0 +1,34 @@
+//
+// Copyright 2015-2099 Hong Miao. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package conf
+
+const (
+	KEY_ACTION = "action"
+	KEY_PATH = "path"
+	KEY_TOKEN = "token"
+	KEY_DIR = "dir"
+	KEY_OLD_DIR = "old_dir"
+	KEY_NEW_DIR = "new_dir"
+)
+
+const (
+	ACTION_LOGIN = "login"
+	ACTION_GET_SERVER_FILE_LIST = "get_server_file_list"
+	ACTION_MKDIR = "mkdir"
+	ACTION_RMFILE = "rm_file"
+	ACTION_RENAME = "rename"
+	ACTION_GET_STORAGE_INFO = "get_storage_info"
+)
